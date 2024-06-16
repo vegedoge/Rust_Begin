c83f6430ad45c286f2e82a831517d269e688f4aa
blake <yx.wu.work@gmail.com>
2024-06-16 12:37:21 +0800

    ref added;

--- src/bin/ref.rs
+++ src/bin/ref.rs
@@ -13,4 +13,19 @@ fn main(){
     println!("s is {}, *p3 is {}", *p3, *p3);
     // s is not allowed to be visited here
     // we know that prinln! s needs a ref to s
+    let c = '中';
+    let r1 = &c;
+    let ref r2 = c;
+
+    assert_eq!(*r1, *r2);
+    
+    // assert adress equal
+    assert_eq!(get_addr(r1),get_addr(r2));
+    println!("& and ref works the same");
+
 }
+fn get_addr(r: &char) -> String {
+    format!("{:p}", r)
+}
+
+
