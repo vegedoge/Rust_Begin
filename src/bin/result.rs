c017b52ac43fc754e7012742dfc7aacb9646ce22
blake <yx.wu.work@gmail.com>
2024-07-26 12:07:39 +0800

    add hello.txt for Error test

--- src/bin/result.rs
+++ src/bin/result.rs
@@ -1,14 +1,20 @@
 
 // practice for panic and result<T, E>
-use std::fs::File;
+use std::{fs::File, io::ErrorKind};
 
 fn main(){
     let f = File::open("hello.txt");
 
     let f = match f {
         Ok(file) => file,
-        Err(error) => {
-            panic!("Problem occured while opening {:?}", error)
+
+        Err(error) => match error.kind(){
+            ErrorKind::NotFound => match File::create("hello.txt"){
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating file {:?}", e),
+            },
+            other_error => panic!("Problem opening the file {:?}", other_error)
         },
+        
     };
 }
