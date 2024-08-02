1d8e3ae414735d32dc0f4a68cd4d34d308df5e24
blake <yx.wu.work@gmail.com>
2024-08-02 22:47:25 +0800

    Practice for closure

---
+++ src/bin/closure_cacher.rs
@@ -0,0 +1,44 @@
+// cacher practice for Closure
+struct Cacher<T, E>
+where
+    T: Fn(E) -> E,
+    E: Copy
+{
+    query: T,
+    value: Option<E>,
+}
+
+impl <T, E> Cacher<T, E>
+where 
+    T: Fn(E) -> E,
+    E: Copy
+{
+    fn new(query: T) -> Cacher<T, E> {
+        Cacher {
+            query,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: E) -> E {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.query)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut c = Cacher::new(|a| a);
+
+    let _v1 = c.value(1);
+    let v2 = c.value(2);
+
+    assert_eq!(v2, 1);
+
+}
+
