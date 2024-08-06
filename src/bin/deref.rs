283cde0e45c615f0d4edfea1f42434fd2f1d613c
blake <yx.wu.work@gmail.com>
2024-08-06 22:05:47 +0800

    Deref: Add Deref_mut

--- src/bin/deref.rs
+++ src/bin/deref.rs
@@ -1,10 +1,12 @@
 // Trait: Deref for intelligent pointer
 use std::ops::Deref;
-struct MyBox<T>(T);
+struct MyBox<T> {
+    v: T
+}
 
 impl<T> MyBox<T> {
     fn new(x: T) -> MyBox<T> {
-        MyBox(x)
+        MyBox{v: x}
     }
 }
 
@@ -12,8 +14,25 @@ impl<T> Deref for MyBox<T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
-        &self.0
+        &self.v
     }
+}
+
+use::std::ops::DerefMut;
+
+impl<T> DerefMut for MyBox<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.v
+    }
+}
+
+fn display(s: &str) {
+    println!("{}", s);
+}
+
+fn display_mut(s: &mut String) {
+    s.push_str("world");
+    println!("{}", s);
 }
 
 fn main() {
@@ -21,4 +40,12 @@ fn main() {
 
     // struct without Deref will cause error
     assert_eq!(5, *x);
+
+    // auto Deref: only be done to &var
+    let s = Box::new(String::from("Hello, Rust"));
+    display(&s);
+    
+    let mut s2 = MyBox::new(String::from("CPP, "));
+    display_mut(&mut s2);
+    let _s3 = s.to_string();
 }
