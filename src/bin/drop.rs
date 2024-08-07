f30277fe8a8c7543cf6f79e7099f25e2d3fbfaab
blake <yx.wu.work@gmail.com>
2024-08-07 11:42:46 +0800

    study for Drop

---
+++ src/bin/drop.rs
@@ -0,0 +1,42 @@
+// Drop trait practice
+struct HasDrop1;
+struct HasDrop2;
+
+impl Drop for HasDrop1 {
+    fn drop(&mut self) {
+        println!("Dropping HasDrop1");
+    }
+}
+
+impl Drop for HasDrop2 {
+    fn drop(&mut self) {
+        println!("Dropping HasDrop2");
+    }
+}
+
+struct HasTwoDrops {
+    one: HasDrop1,
+    two: HasDrop2,
+}
+
+impl Drop for HasTwoDrops {
+    fn drop(&mut self) {
+        println!("Dropping HasTwoDrops");
+    }
+}
+struct Foo;
+
+impl Drop for Foo {
+    fn drop(&mut self) {
+        println!("Dropping Foo");
+    }
+}
+
+fn main() {
+    let _x = HasTwoDrops {
+        one: HasDrop1,
+        two: HasDrop2,
+    };
+    let _foo = Foo;
+    println!("Running");
+}
